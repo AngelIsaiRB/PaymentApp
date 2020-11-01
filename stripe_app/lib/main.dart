c01e9127c183a4fd66ad4debb1a46c3d5b4c4f64
Anegelisai <63125934+Anegelisai@users.noreply.github.com>
2020-10-31 23:51:17 -0600

    Bloc fns

--- stripe_app/lib/main.dart
+++ stripe_app/lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:stripe_app/bloc/pagar/pagar_bloc.dart';
 import 'package:stripe_app/pages/home_page.dart';
 import 'package:stripe_app/pages/pago_completo.dart';
 import 'package:stripe_app/pages/tarjeta_page.dart';
@@ -8,18 +10,23 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Pagos app',
-      initialRoute: "homepage",
-      routes: {
-        "homepage" : (_)=>HomePage(),
-        "tarjetaPage": (_) => TarjetaPage(),
-        "pagoPage"  : (_) => PagoPage()
-      },
-      theme: ThemeData.light().copyWith(
-        primaryColor: Color(0xff284879),
-        scaffoldBackgroundColor: Color(0xff21232a)
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(create: (BuildContext context)=>PagarBloc(),)
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Pagos app',
+        initialRoute: "homepage",
+        routes: {
+          "homepage" : (_)=>HomePage(),
+          "tarjetaPage": (_) => TarjetaPage(),
+          "pagoPage"  : (_) => PagoPage()
+        },
+        theme: ThemeData.light().copyWith(
+          primaryColor: Color(0xff284879),
+          scaffoldBackgroundColor: Color(0xff21232a)
+        ),
       ),
     );
   }
